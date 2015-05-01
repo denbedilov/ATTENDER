e84927c1382b6319215a5739d722eb53d29213f5
olesyash <ilana.olesya@gmail.com>
2015-05-01 20:34:29 +0300

    mend

---
+++ server/attender-mobile/DAL.py
@@ -0,0 +1,103 @@
+__author__ = 'olesya'
+
+#This class is suppose to access to DB and transfer answers to other classe
+#For the time the server is under development it will retrieve fake data
+
+from google.appengine.ext import ndb
+from models.User import User
+from models.Event import Event
+from models.Attendings import Attendings
+import datetime
+
+class DAL():
+    us_list = []
+    ev_list = []
+    user1 = User()
+    user1.user_name = "oles_ka"
+    user1.email = "someemail.gmail.com"
+    user1.user_password = "12345"
+    user1.first_name = "Olesya"
+    user1.last_name = "Shapira"
+
+    us_list.append(user1)
+
+    event1 = Event()
+    event1.address = "somesdress"
+    event1.date = datetime.date(15, 5, 1)
+    event1.description = "some description"
+    event1.name = "python meeting"
+    event1.owner = "Olesya"
+    event1.time = datetime.time(13, 0)
+
+    ev_list.append(event1)
+
+    attendings1 = Attendings()
+    attendings1.event_id = attendings1.key
+    attendings1.user_id = attendings1.key
+
+
+    def get_user_details(self, type):
+        if type == "user_name":
+            return self.user1.user_name
+        elif type == "first_name":
+            return self.user1.first_name
+        elif type == "last_name":
+            return self.user1.last_name
+        elif type == "email":
+            return self.user1.email
+        elif type == "user_password":
+            return self.user1.user_password
+
+    def get_event_details(self, type):
+        if type == "address":
+            return self.event1.address
+        elif type == "date":
+            return self.event1.date
+        elif type == "description":
+            return self.event1.description
+        elif type == "name":
+            return self.event1.name
+        elif type == "owner":
+            return self.event1.owner
+        elif type == "time":
+            return self.event1.time
+
+    def get_attendings(self, type):
+        if type == "event_id":
+            return self.attendings1.event_id
+        elif type == "user_id":
+            return self.attendings1.user_id
+
+    def set_user_details(self, un, em, psw, fn=None, ls=None):
+        user1 = User()
+        user1.user_name = un
+        user1.email = em
+        user1.user_password = psw
+        user1.first_name = fn
+        user1.last_name = ls
+        self.us_list.append(user1)
+        print self.us_list
+        return self.us_list
+        #user1.put()
+
+    def set_event_details(self, name, date, time, add, descr, own):
+        event1 = Event()
+        event1.name = name
+        event1.date = date
+        event1.time = time
+        event1.address = add
+        event1.description = descr
+        event1.owner = own
+        self.ev_list.append(event1)
+        print self.ev_list
+        return self.ev_list
+        #event1.put()
+
+    def set_attendings(self, u_key, e_key):
+        attendings1 = Attendings()
+        attendings1.user_id = u_key
+        attendings1.event_id = e_key
+
+        #attendings1.put()
+
+
---
+++ server/attender-mobile/tests/DBTest.py
@@ -0,0 +1,22 @@
+__author__ = 'olesya'
+
+import unittest
+from models.User import User
+from DAL import DAL
+
+
+class UserDetails(unittest.TestCase):
+    def test_get_name(self):
+        d = DAL()
+        self.assertEqual(d.get_user_details("user_name"),"oles_ka")
+
+    def test_set_name(self):
+        d = DAL()
+        us_list = d.set_user_details("itamar", "sn@dd.com", "123")
+        self.assertEqual(us_list.pop().user_name ,"itamar")
+
+def main():
+    unittest.main()
+
+if __name__ == "__main__":
+    main()
