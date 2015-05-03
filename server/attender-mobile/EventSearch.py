582a23f6adfcad0ee28ed88c51fb6b42f363d0b8
olesyash <ilana.olesya@gmail.com>
2015-05-03 23:31:10 +0300

    Meetup API pull
    Pulling and printing the requested for us data that get from meetup API

--- server/attender-mobile/EventSearch.py
+++ server/attender-mobile/EventSearch.py
@@ -1,10 +1,8 @@
 # __author__ = 'itamar'
 from __future__ import unicode_literals
-
-
 import webapp2
 import requests
-# from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup
 import time
 import codecs
 import sys
@@ -16,52 +14,90 @@ from models.Attendings import Attendings
 import json
 
 
+
+
+# Meetup.com documentation here: http://www.meetup.com/meetup_api/docs/2/groups/
+
 UTF8Writer = codecs.getwriter('utf8')
+#parse_json = lambda s: simplejson.loads(s.decode('utf8'))
+
+
 sys.stdout = UTF8Writer(sys.stdout)
 
 URL_PATTERN =  "https://api.meetup.com/find/open_events?"
+
+#TODO: Scheduale for each day to pull events and save in local DB. Pull new events every day
 API_KEY = "185c2b3e44c4b4644365a3022d5a2f"
+#TODO: close connection
 
 class SearchHandler(webapp2.RequestHandler):
-
-    def query(self, city = None, topic = None, hour = None):
-        if city!=None:
-            cities = [(str(city),"IL")]#case choise was given
-        else:#'stock settings'
-            cities =[("Jerusalem","IL"),("Tel Aviv","IL"),("Haifa","IL")]
-        # If you want to change the cities to search, do so below
+    def query(self, city=None, topic=None, datetime=None):
+        if city is not None:
+            cities = [(str(city),"IL")] #case choise was given
+        else: #'default cities'
+            cities =[("Jerusalem", "IL"),("Tel Aviv", "IL"),("Haifa", "IL")]
 
         for (city, state) in cities:
             per_page = 200
             results_we_got = per_page
             offset = 0
+
+            request = {"sign": "true", "country": "IL", "city": city, "state": state, "key": API_KEY,
+                                       "page": per_page, "offset": offset, "fields":"event_hosts"}
+            if topic is not None:
+                t = {"topic": topic}
+                request.update(t)
+            if datetime is not None:
+                ti = {"time": datetime}
+                request.update(ti)
+
             while (results_we_got == per_page):
-                # Meetup.com documentation here: http://www.meetup.com/meetup_api/docs/2/groups/
-                # You can change search perimeter around each city by changing the "radius" parameter
-                response=get_results({"sign":"true","country":"IL","topic":topic, "city":city, "state":state, "key":API_KEY, "page":per_page, "offset":offset })
+                response = get_results(request)
                 time.sleep(1)
                 offset += 1
-                results_we_got = response['meta']['count']
-                # print response
-                for group in response['results']:
-                    category = ""
-                    if "topic" in group:
-                        category = group['topic']['name']
-                    print "\n"+"," .join(map(unicode, [city, group['name'].replace(","," "), group['description'], category]))
+
+               # print response
+                print 100*"-"
+                for res in response['results']:
+                    print res['name']  #name
+                    print res['time'] #miliseconds
+                    try: #Address
+                        address = res['venue']['address_1']
+                        print address
+                    except:
+                        pass
+                    print res['yes_rsvp_count'] #Attendees
+                    print res['description'] #description
+                    print res['event_url'] #link
+
+                    for host in res['event_hosts']: #Owner
+                        print host['member_name']
+
+
+
+
+
+                print 100*"-"
+                # if response is not None:
+                    # results_we_got = response['meta']['count']
+                    # for res in response['results']:
+                    #     category = ""
+                    #     if "topic" in res:
+                    #         category = res['topic']['name']
+                    #     print "\n"+"," .join(map(unicode, [city, res['name'].replace(","," "), res['description'], category]))
 
  # group['topic'], category,group['members'],group['status'],group.get('organizer_id',""),,group.get('who',"").replace(","," ")
-        time.sleep(1)
+                time.sleep(1)
+
 
 def get_results(params):
-
     request = requests.get("http://api.meetup.com/2/open_events",params=params)
     data = request.json()
-
     return data
 
 if __name__=="__main__":
     temp = SearchHandler()
-    temp.query("Jerusalem", "Games")
+    temp.query("Jerusalem")
 
 
 search = webapp2.WSGIApplication([
