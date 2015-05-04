c8a2a7de00a1f7c9a76d73434e6d1c05e6301686
Shai Pe'er <shaip86@gmail.com>
2015-05-04 12:55:29 +0300

    Update search page and Json reading
    Update search page and Json reading

---
+++ app/src/main/java/com/example/rita/attender/ChatPageActivity.java
@@ -0,0 +1,70 @@
+package com.example.rita.attender;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import java.sql.Date;
+import java.util.ArrayList;
+
+public class ChatPageActivity extends ActionBarActivity
+{
+    AttenderBL bl;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        bl = new AttenderBL();
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat_page);
+        ListView listView = (ListView) findViewById(R.id.listView);
+
+        ArrayList<Event> events = bl.getEvents("type", "fds", "fdsf");  ///!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+        EventAdapter adapter = new EventAdapter(this, events);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent myIntent = new Intent(getApplicationContext(), Event_Page_Activity.class);
+                startActivity(myIntent);
+            }
+        });
+    }
+    public void eventsPressed(View v)
+    {
+        Intent intent=new Intent(this,searchEventActivity.class);
+        startActivity(intent);
+    }
+    public void explorePressed(View v)
+    {
+        Intent intent=new Intent(this,CalendarPageActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_chat_page, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
