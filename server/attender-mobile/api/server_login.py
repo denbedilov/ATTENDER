d276a33fc9ea25ad7cf9c54b6024d34e62c19436
itamar sharify <itamarsharify@gmail.com>
2015-05-18 20:39:26 +0300

    Fb login added
    this is the first version, updates will be up soon

--- server/attender-mobile/api/server_login.py
+++ server/attender-mobile/api/server_login.py
@@ -5,34 +5,27 @@ import json
 import logging
 import webapp2
 import facebook
+import hashlib
+import jinja2
+import hmac
 from DAL import DAL
 sys.path.insert(0, 'lib')  #we need this line in order to make libraries imported from lib folder work properly
 import requests
 
 FACEBOOK_APP_ID = "683953828381840"
-FACEBOOK_APP_SECRET = "294d93ab1301dab751e5497bb318fb68"
-FACEBOOK_URL = "https://graph.facebook.com/oauth/access_token?"
-config = {}
-config['webapp2_extras.sessions'] = dict(secret_key='')
 
 class APILoginHandler(webapp2.RequestHandler):
-    mydb = DAL()
+
     def get(self):
         received = False
         id = self.request.get("id").encode('ascii', 'ignore')
         token = self.request.get("token").encode('ascii', 'ignore')
         if id == "" or  token == "":
             received = False
-            #build a Fb auth
         else:
-            received = True
-            request = {}
-            # graph = facebook.GraphAPI(token)
-            # profile = graph.get_object("me")
-            # friends = graph.get_connections("me", "friends")
-            #
-            # friend_list = [friend['name'] for friend in friends['data']]
-            self.mydb.set_user_details(int(id), "test","hey")
+            received = validate_fb_login(id,token)
+            logging.info(received)
+
         self.post(received)
 
     def post(self, received):
@@ -40,18 +33,31 @@ class APILoginHandler(webapp2.RequestHandler):
             self.response.set_status(400)
             self.response.write("ERROR: Missing parameters")
             return
-        # elif received == 2:
-        #     self.response.write("Session Aouth Failed")
+        elif received == 1:
+             self.response.write("Session Aouth Failed")
         else:
             self.response.set_status(200)
             self.response.write("OK")
             return
 
 
+def validate_fb_login(id,access_token):
+    graph = facebook.GraphAPI(access_token)
+    user = graph.get_object("me")
+    _id = user['id']
+    if id == _id:
+        mydb = DAL()
+        mydb.set_user_details(id,user['name'],user['last_name'])
+        return True
+    else:
+        return 1
+
 def get_results(request_url, params):
-    request = requests.get(request_url, params=params)
+    request = requests.get(request_url, params=params, verify=True)
     data = request.json()
     return data, request.status_code
+
+
 login = webapp2.WSGIApplication([
     ('/login', APILoginHandler)
 ], debug=True)
