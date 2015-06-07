0346b033ede4381e32bf66b9910ecf52ce64b0de
itamar sharify <itamarsharify@gmail.com>
2015-06-07 13:04:24 +0300

    updated google login

--- server/attender-mobile/api/google_login.py
+++ server/attender-mobile/api/google_login.py
@@ -10,8 +10,19 @@ class GoogleLoginHandler(webapp2.RequestHandler):
     @login_required
     def get(self):
         user = users.get_current_user()
+        if user is not None:
+            self.post(True)
+        else:
+            self.post(False)
         self.response.out.write('Hello, ' + user.nickname())
 
+    def post(self,response):
+        if response is not None:
+            self.response.set_status("200")
+            self.response.write(response)
+        else:
+            self.response.set_status("400")
+            self.response.write("Aouth Failed")
 
 googlelogin = webapp2.WSGIApplication([
     ('/googlelogin', GoogleLoginHandler)
