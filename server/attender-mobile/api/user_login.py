f2c850115a29c73d74c863814baf4e379c1b6456
olesyash <ilana.olesya@gmail.com>
2015-06-09 00:34:13 +0300

    Check email valid
    fixed

--- server/attender-mobile/api/user_login.py
+++ server/attender-mobile/api/user_login.py
@@ -5,63 +5,85 @@ import logging
 import webapp2
 from DAL import DAL
 from google.appengine.api import mail
+import re
+
+
+qtext = '[^\\x0d\\x22\\x5c\\x80-\\xff]'
+dtext = '[^\\x0d\\x5b-\\x5d\\x80-\\xff]'
+atom = '[^\\x00-\\x20\\x22\\x28\\x29\\x2c\\x2e\\x3a-\\x3c\\x3e\\x40\\x5b-\\x5d\\x7f-\\xff]+'
+quoted_pair = '\\x5c[\\x00-\\x7f]'
+domain_literal = "\\x5b(?:%s|%s)*\\x5d" % (dtext, quoted_pair)
+quoted_string = "\\x22(?:%s|%s)*\\x22" % (qtext, quoted_pair)
+domain_ref = atom
+sub_domain = "(?:%s|%s)" % (domain_ref, domain_literal)
+word = "(?:%s|%s)" % (atom, quoted_string)
+domain = "%s(?:\\x2e%s)*" % (sub_domain, sub_domain)
+local_part = "%s(?:\\x2e%s)*" % (word, word)
+addr_spec = "%s\\x40%s" % (local_part, domain)
+email_address = re.compile('\A%s\Z' % addr_spec)
+
+
+
+
 
 class UserLoginHandler(webapp2.RequestHandler):
     def get(self):
-        email = self.request.get("email").encode('ascii', 'ignore')
+        email = self.request.get("email")#.encode('ascii', 'ignore')
         hashed_password = self.request.get("password")
         first_name = self.request.get("firstname")
         last_name = self.request.get("lastname")
-        if not mail.is_email_valid(email):
-            self.post(-4)
-            return
+
+        mydb = DAL()
 
         if email is "" or hashed_password is "":
             self.post(-1)
-            return
-        # if first_name is "":
-        #     self.post(-2)
-        # if last_name is "":
-        #     self.post(-3)
-
-        mydb = DAL()
-        user = mydb.user_login(email,hashed_password)
-        if user is False or user is 2:
-            if first_name is not  "" and last_name is not "":
-                user = mydb.register(email, hashed_password, first_name, last_name)
-                logging.info("in register  "+str(user))
-            elif first_name is not "" and last_name is "":
-                self.post(-3)
-            elif first_name is "" and last_name is not "":
-                self.post(-2)
+        elif email is not "" and hashed_password is not "":
+            isvalid = isValidEmailAddress(email)
+            logging.info(isvalid)
+            if not isvalid:
+                self.post(-4)
             else:
-                self.post(False)
-        logging.info("after if   "+str(user))
-        self.post(user)
+                user = mydb.user_login(email,hashed_password)
+                if user is False or user is 2:
+                    if first_name is not "" and last_name is not "":
+                        user = mydb.register(email, hashed_password, first_name, last_name)
+                        logging.info("in register  " + str(user))
+                    elif first_name is not "" and last_name is "":
+                        user = -3
+                    elif first_name is "" and last_name is not "":
+                        user = -2
+                    else:
+                        user = False
+                self.post(user)
 
     def post(self,response):
         if response is -4:
             self.response.set_status(403)
             self.response.write("Invalid mail")
-        if response is -3:
+        elif response is -3:
             self.response.set_status(402)
             self.response.write("Missing Last Name")
-        if response is -2:
+        elif response is -2:
             self.response.set_status(401)
             self.response.write("Missing First Name")
-        if response is -1:
+        elif response is -1:
             self.response.set_status(400)
             self.response.write("Missing Parameters")
-        if response is False:
+        elif response is False:
             self.response.set_status(501)
             self.response.write("User Not Exist")
-        if response is 1:
+        elif response is 1:
             self.response.set_status(500)
             self.response.write("Wrong Password")
         else:
             self.response.set_status(200)
-            logging.info("in else")
             self.response.write(response)
+
+def isValidEmailAddress(email):
+    if email_address.match(email):
+        return True
+    else:
+        return False
 
 user = webapp2.WSGIApplication([
     ('/userlogin', UserLoginHandler)
